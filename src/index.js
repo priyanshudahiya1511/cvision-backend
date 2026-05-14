6cc5f64ced71c3c24fe304d4f24d39999eae384f
Priyanshu Dahiya <priyanshudahiya1234@gmail.com>
2026-05-14 15:36:25 +0530

    fix: force ipv4first dns resolution app-wide

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
 import { app } from "./app.js";
@@ -9,11 +12,11 @@ dotenv.config({
 const Port = process.env.PORT || 5000;
 
 connectDb()
-.then(() => {
-    app.listen(Port, () => {
-        console.log(`Server is running on Port no. ${Port}`);
+    .then(() => {
+        app.listen(Port, () => {
+            console.log(`Server is running on Port no. ${Port}`);
+        });
     })
-})
-.catch((err) => {
-    console.log("MongoDb connection error", err);
-  });
+    .catch((err) => {
+        console.log("MongoDb connection error", err);
+    });
